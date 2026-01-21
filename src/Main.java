4ff96d59216d4e0235a643bcd4eb794401496706
“Aleksey <a.glazovv77@gmail.com>
2026-01-21 16:44:37 +0300

    Test-demo-branch in class Lesson1 method stringSay create static

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import glazov.Less2;
 import glazov.Lesson1;
 
+import static glazov.Lesson1.stringSay;
+
 public static void main(String[] args) {
 
         Lesson1 lesson1 = new Lesson1();
         System.out.println("Summa = " + lesson1.sum(587, 1258));
 
-        Lesson1 lesson2 = new Lesson1();
-        lesson2.stringSay("Hello, Alex!");
+        stringSay("Hello, Alex!");
 
         Less2.sayHello();
 
